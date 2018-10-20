787719a60bbba9d8c58c071bd8b2c8f97c8fbcd3
Robin Aggleton <robin.aggleton@cern.ch>
2018-10-20 13:06:11 +0200

    Add setup.py to allow installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='compareRootTrees',
+    version='0.1',
+    description="Produce plots for every branch variable comparing 2 ROOT TTrees",
+    author='Robin Aggleton',
+    author_email='',
+    url='https://github.com/raggleton/compareRootTrees',
+    scripts=['compareRootTrees.py']
+)
